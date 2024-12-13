38bf4ff9423ed6c51549878c16b29e58a1ad0355
Fabiola Ferreira <fabiola.lopes@futcorp.com.br>
2024-12-13 11:54:24 -0300

    Feat: Entrada de dados, estrutura de decisão

---
+++ screen-match.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "Bem-Vindo(a) ao screen match!\n";
+
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = $argv[1] ?? 2022;  
+
+$somaDeNotas = 9;
+$somaDeNotas += 6;
+$somaDeNotas += 8; 
+$somaDeNotas += 7.5;
+$somaDeNotas += 5;
+
+$notaFilme = $somaDeNotas / 5;
+$planoPrime = true; 
+
+$inlcuidoNoPlano = $planoPrime || $anoLancamento < 2020;
+
+echo "Nome do filme: " . $nomeFilme . "\n"; 
+echo "Nota do filme: $notaFilme\n";
+echo "Ano de lançamento: $anoLancamento\n";
+
+if ($anoLancamento > 2022) {
+    echo "Esse filme é um lançamento"; 
+}
